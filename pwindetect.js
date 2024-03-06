61379ac2c9638d246f3ea9db8a8af7e587f3f52c
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-03-06 10:34:42 -0700

    Update pwindetect.js

--- pwindetect.js
+++ pwindetect.js
@@ -1,41 +1,15 @@
-function pwindetection() {
-	
-	var gameover = false;
+function pwindetect() {
 	
 	for (d = 1; d < 7; d++) {
 		if (pholes[d] != 0) {
-			empty1s = 0;
-			for (d = 8; d < 14; d++) {
-				if (pholes[d] != 0) {
-					empty2s = 0;
-					return 0;
+			for (f = 8; f < 14; f++) {
+				if (pholes[f] != 0) {
+					return false;
 				}
-				else {
-					empty2s++;
-				}
-			}
-		}
-		else {
-			empty1s++;
+			}//checks other side for filled hole
+			return true;
 		}
 	}
-	
-	if (empty1s >= 6) {
-		for (d = 8; d < 14; d++) {
-			pholes[7] += pholes[d];
-			gameover = true;
-		}
-	}
-	else if (empty2s >= 6) {
-		for (d = 1; d < 7; d++) {
-			pholes[0] += pholes[d];
-			gameover = true;
-		}
-	}
-	
-	empty1s = 0;
-	empty2s = 0;
-	
-	return gameover;
+	return true;
 	
 }
