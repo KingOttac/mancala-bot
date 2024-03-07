df37e5a8ec3bd3ccc7d839f020abdd286a3b79ef
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-03-06 23:35:52 -0700

    Update ai.js (alpha beta incoming soon)

--- ai.js
+++ ai.js
@@ -1,10 +1,11 @@
-function ai() {
+function ai(aturn) {
 
+	//if aturn == 1 then returns a value for player 2, if aturn == 0 then player 1
 	//note that this plays on turn == 1, returns are from 6-1 from left to right of bottom row, goal is holes[0]
 	//returned values are printed
 	//nonai plays on turn == 0, returns are 7-12 from left to right of top row, goal is holes[7]
 	
-	let returnedarr = [holes[7],holes[7],holes[7],holes[7],holes[7],holes[7]];
+	let returnedarr = [holes[aturn*7],holes[aturn*7],holes[aturn*7],holes[aturn*7],holes[aturn*7],holes[aturn*7]];
 	let depth = 6;//depth > 0
 	
 	for (a = 0; a < pow(6,depth); a++) {
@@ -17,12 +18,12 @@ function ai() {
 		for (b = 0; b < 14; b++) {
 			pholes[b] = holes[b];
 		}
-		let pturn = 1;
+		let pturn = aturn;
 		for (b = 0; b < depth && pwindetect() == false; b++) {
 			
 			let checkingvar;
 			if (pturn == 0) {
-				checkingvar = pmovepiece(13-movelist[b]);
+				checkingvar = pmovepiece(movelist[b]+8);
 			}
 			else if (pturn == 1) {
 				checkingvar = pmovepiece(movelist[b]+1);
@@ -41,9 +42,12 @@ function ai() {
 			for (d = 8; d < 14; d++) {
 				pholes[7] += pholes[d];
 			}
+			for (d = 1; d < 7; d++) {
+				pholes[0] += pholes[d];
+			}
 		}//simulates ending for better result
-		if (pholes[7] > returnedarr[movelist[0]]) {
-			returnedarr[movelist[0]] = pholes[7];
+		if (pholes[aturn*7] > returnedarr[movelist[0]]) {
+			returnedarr[movelist[0]] = pholes[aturn*7];
 		}
 		
 	}//pov you are a seximal for loop
@@ -55,8 +59,8 @@ function ai() {
 	sort(returnedarr);
 	for (a = 0; a < 6; a++) {
 		for (b = 0; b < 6; b++) {
-			if (returnedarr[a] == usra[b] && holes[b+1] != 0) {
-				return b+1;
+			if (returnedarr[a] == usra[b] && holes[b+pow(8,1-aturn)] != 0) {
+				return b+pow(8,1-aturn);
 			}
 		}
 	}
