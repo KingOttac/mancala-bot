924ef2f51c7f930d4806e011ab5d82c4e01bd387
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-03-06 23:36:30 -0700

    pmovepiece changed as well

--- pmovepiece.js
+++ pmovepiece.js
@@ -1,10 +1,9 @@
 function pmovepiece(hole) {
-	
+		
+	let turnside;
 	if (pholes[hole] == 0) {
 		return -1;
-	}
-		
-	rcounter = 0;
+	}//bug catch
 	
 	if (hole < 7) {
 		turnside = 1;
@@ -12,66 +11,54 @@ function pmovepiece(hole) {
 	else if (hole > 7) {
 		turnside = 0;
 	}
-	
-	k = pholes[hole];
-	
+		
 	if (turnside == 0) {
+		let cycler = 0;
+		let k = pholes[hole];
 		pholes[hole] = 0;
-		for (g = 1; g <= k; g++) {
-			if (hole-(g - 14*rcounter) != 0) {
-				pholes[hole-(g - 14*rcounter)]++;
+		for (g = k; g > 0; g--) {
+			cycler++;
+			if (cycler > hole) {
+				cycler = hole-13;
 			}
-			else {
-				rcounter++;
+			if (hole-cycler != 0) {
+				pholes[hole-cycler]++;
+				if (g == 1 && pholes[hole-cycler] == 1 && hole-cycler > 7 && pholes[14-(hole-cycler)] != 0) {
+					pholes[7] += 1 + pholes[14-(hole-cycler)];
+					pholes[14-(hole-cycler)] = 0;
+					pholes[hole-cycler] = 0;
+					return 1;
+				}//captures
+				else if (g == 1 && hole-cycler == 7) {
+					return 0;
+				}//free turn
 			}
 		}
-		
-		if (pholes[hole-(g - 14*rcounter)+1] == 1 && hole-(g - 14*rcounter)+1 > 7 && pholes[14-(hole-(g - 14*rcounter)+1)] != 0) {
-			pholes[7] += 1 + pholes[14-(hole-(g - 14*rcounter)+1)];
-			pholes[14-(hole-(g - 14*rcounter)+1)] = 0;
-			pholes[hole-(g - 14*rcounter)+1] = 0;
-			return 1;
-		}//capture
-		else if (hole-(g - 14*rcounter)+1 == 7) {
-			return 0;
-		}//extra turn
-		else {
-			return 1;
-		}//normal turn
-
-		rcounter = 0;
-
+		return 1;
 	}
+	
 	else if (turnside == 1) {
+		let cycler = 0;
+		let k = pholes[hole];
 		pholes[hole] = 0;
-		rcounter = 0;
-		for (g = 1; g <= k; g++) {
-			if (hole-(g - 14*rcounter) != 0 && hole-(g - 14*rcounter) != 7) {
-				pholes[hole-(g - 14*rcounter)]++;
+		for (g = k; g > 0; g--) {
+			cycler++;
+			if (cycler > hole) {
+				cycler = hole-13;
 			}
-			else if (hole-(g - 14*rcounter) == 0) {
-				pholes[hole-(g - 14*rcounter)]++;
-				if (hole == k) {}
-				else {
-					rcounter++;
-				}
+			if (hole-cycler != 7) {
+				pholes[hole-cycler]++;
+				if (g == 1 && pholes[hole-cycler] == 1 && hole-cycler < 7 && pholes[14-(hole-cycler)] != 0) {
+					pholes[0] += 1 + pholes[14-(hole-cycler)];
+					pholes[14-(hole-cycler)] = 0;
+					return 0;
+				}//captures
+				else if (g == 1 && hole-cycler == 0) {
+					return 1;
+				}//free turn
 			}
 		}
-		
-		if (pholes[hole-(g - 14*rcounter)+1] == 1 && hole-(g - 14*rcounter)+1 < 7 && pholes[14-(hole-(g - 14*rcounter)+1)] != 0 && hole-(g - 14*rcounter)+1 != 0) {
-			pholes[0] += 1 + pholes[14-(hole-(g - 14*rcounter)+1)];
-			pholes[14-(hole-(g - 14*rcounter)+1)] = 0;
-			pholes[hole-(g - 14*rcounter)+1] = 0;
-			return 0;
-		}//capture
-		else if (hole-(g - 14*rcounter)+1 == 0) {
-			return 1;
-		}//extra turn
-		else {
-			return 0;
-		}//normal turn
-		rcounter = 0;
-		
+		return 0;
 	}
 	
 }
