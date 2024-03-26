eb58cd25a4161a7373585561a38d63aa0158259e
KingOttac <155270221+KingOttac@users.noreply.github.com>
2024-03-25 21:35:53 -0700

    nonai.js now works for both players

--- nonai.js
+++ nonai.js
@@ -1,13 +1,17 @@
-function nonai() {
+function nonai(aturn) {
 	if (mouseIsPressed == true) {
 		for (q = 0; q < 6; q++) {
 			let left = 0.254*windowWidth+(windowWidth/10)*q-windowWidth/24;
 			let right = 0.254*windowWidth+(windowWidth/10)*q+windowWidth/24;
-			let top = 0.254*windowHeight-windowWidth/24;
-			let floor = 0.254*windowHeight+windowWidth/24;
-			if (mouseX > left && mouseX < right) {
-				movepiece(q+8);
-				break;
+			let top = 0.254*windowHeight-windowWidth/24 + aturn*(windowHeight-2*0.254*windowHeight);
+			let floor = 0.254*windowHeight+windowWidth/24 + aturn*(windowHeight-2*0.254*windowHeight);
+			if (mouseX > left && mouseX < right && mouseY > top && mouseY < floor) {
+				if (aturn == 0) {
+					return q+8;
+				}
+				else {
+					return 6-q;
+				}
 			}
 		}
 	}
